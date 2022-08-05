fbc760532e861f518dd05385b827ab18d2c856a3
Zakhar Khakhalin <khakhalinz@yandex.ru>
2022-08-05 09:56:40 +0300

    Created console app

---
+++ iAge.ConsoleApp/ConsoleApp/CommandLineHandler.cs
@@ -0,0 +1,237 @@
+﻿using System.CommandLine;
+using System.CommandLine.Parsing;
+using iAge.ConsoleApp.Models;
+
+namespace iAge.ConsoleApp
+{
+    internal class CommandLineHandler
+    {
+        private readonly RootCommand rootCommand;
+
+        public CommandLineHandler()
+        {
+            rootCommand = new RootCommand("Provides basic model storage manipulation procedures.");
+        }
+
+        public void RunCommand(string[] args)
+        {
+            rootCommand.Invoke(args);
+        }
+
+        public event ModelEventHandler<Employee> EmployeeAddEvent = default!;
+        public event ModelEventHandler<Employee> EmployeeUpdateEvent = default!;
+        public event ModelEventHandler<Employee> EmployeeGetEvent = default!;
+        public event ModelEventHandler<Employee> EmployeeDeleteEvent = default!;
+        public event ModelEventHandler<Employee> EmployeeGetAllEvent = default!;
+
+        public RootCommand CreateCommandTree()
+        {
+            var addEmployeeCommand = CreateAddEmployeeCommandBranch();
+            var updateEmployeeCommand = CreateUpdateEmployeeCommandBranch();
+            var getEmployeeCommand = CreateGetEmployeeCommandBranch();
+            var deleteEmployeeCommand = CreateDeleteEmployeeCommandBranch();
+            var getAllEmployeeCommand = CreateGetAllEmployeeCommandBranch();
+
+            rootCommand.AddCommand(addEmployeeCommand);
+            rootCommand.AddCommand(updateEmployeeCommand);
+            rootCommand.AddCommand(getEmployeeCommand);
+            rootCommand.AddCommand(deleteEmployeeCommand);
+            rootCommand.AddCommand(getAllEmployeeCommand);
+
+            return rootCommand;
+        }
+
+        private Command CreateAddEmployeeCommandBranch()
+        {
+            var employeeFirstNameOption = CreateSingleTokenOption(name: "FirstName", isRequired: true, description: "");
+            var employeeLastNameOption = CreateSingleTokenOption(name: "LastName", isRequired: true, description: "");
+            var employeeSalaryOption = CreateSingleTokenOption<decimal>(name: "Salary", isRequired: true, description: "");
+
+            var addEmployeeCommand = new Command("-add", "");
+
+            addEmployeeCommand.AddOption(employeeFirstNameOption);
+            addEmployeeCommand.AddOption(employeeLastNameOption);
+            addEmployeeCommand.AddOption(employeeSalaryOption);
+
+            addEmployeeCommand.SetHandler((firstName, lastName, salary) =>
+            {
+                if (firstName == null || lastName == null || salary == null)
+                    return;
+
+                var employee = new Employee(firstName, lastName, salary);
+
+                EmployeeAddEvent.Invoke(this, new ModelEventArgs<Employee>(employee));
+            },
+            employeeFirstNameOption,
+            employeeLastNameOption,
+            employeeSalaryOption);
+
+            return addEmployeeCommand;
+        }
+
+        private Command CreateUpdateEmployeeCommandBranch()
+        {
+            var employeeIdOption = CreateSingleTokenOption<int>(name: "Id", isRequired: true, description: "");
+            var employeeFirstNameOption = CreateSingleTokenOption(name: "FirstName", isRequired: false, description: "");
+            var employeeLastNameOption = CreateSingleTokenOption(name: "LastName", isRequired: false, description: "");
+            var employeeSalaryOption = CreateSingleTokenOption<decimal>(name: "Salary", isRequired: false, description: "");
+
+            var updateEmployeeCommand = new Command("-update", "");
+
+            updateEmployeeCommand.AddOption(employeeIdOption);
+            updateEmployeeCommand.AddOption(employeeFirstNameOption);
+            updateEmployeeCommand.AddOption(employeeLastNameOption);
+            updateEmployeeCommand.AddOption(employeeSalaryOption);
+
+            updateEmployeeCommand.SetHandler((id, firstName, lastName, salary) =>
+            {
+                if (id == null)
+                    return;
+
+                var employee = new Employee(id ?? -1, firstName, lastName, salary);
+
+                EmployeeUpdateEvent.Invoke(this, new ModelEventArgs<Employee>(employee));
+            },
+            employeeIdOption,
+            employeeFirstNameOption,
+            employeeLastNameOption,
+            employeeSalaryOption);
+
+            return updateEmployeeCommand;
+        }
+
+        private Command CreateGetEmployeeCommandBranch()
+        {
+            var employeeIdOption = CreateSingleTokenOption<int>(name: "Id", isRequired: true, description: "");
+
+            var getEmployeeCommand = new Command("-get", "");
+
+            getEmployeeCommand.AddOption(employeeIdOption);
+
+            getEmployeeCommand.SetHandler((id) =>
+            {
+                if (id == null)
+                    return;
+
+                var employee = new Employee(id ?? -1);
+
+                EmployeeGetEvent.Invoke(this, new ModelEventArgs<Employee>(employee));
+            },
+            employeeIdOption);
+
+            return getEmployeeCommand;
+        }
+
+        private Command CreateDeleteEmployeeCommandBranch()
+        {
+            var employeeIdOption = CreateSingleTokenOption<int>(name: "Id", isRequired: true, description: "");
+
+            var deleteEmployeeCommand = new Command("-delete", "");
+
+            deleteEmployeeCommand.AddOption(employeeIdOption);
+
+            deleteEmployeeCommand.SetHandler((id) =>
+            {
+                if (id == null)
+                    return;
+
+                var employee = new Employee(id ?? -1);
+
+                EmployeeDeleteEvent.Invoke(this, new ModelEventArgs<Employee>(employee));
+            },
+            employeeIdOption);
+
+            return deleteEmployeeCommand;
+        }
+
+        private Command CreateGetAllEmployeeCommandBranch()
+        {
+            var getAllEmployeeCommand = new Command("-getall", "");
+
+            getAllEmployeeCommand.SetHandler(() =>
+            {
+                EmployeeGetAllEvent.Invoke(this, new ModelEventArgs<Employee>(new Employee()));
+            });
+
+            return getAllEmployeeCommand;
+        }
+
+        private static Option<T?> CreateSingleTokenOption<T>(string name, bool isRequired = false, string? description = null)
+            where T : struct
+        {
+            var valueToReturnType = typeof(T);
+            var valueToReturn = new T();
+
+            var parseArgument = new ParseArgument<T?>(parseArgumentResult => 
+            {
+                string? tokenValue = GetStringFromSingleTokenOption(parseArgumentResult, "FirstName");
+
+                var parseMethod = valueToReturnType.GetMethod("Parse", new[] { typeof(string) });
+
+                if (parseMethod == null)
+                    throw new Exception($"No provided method [Parse] on type '{valueToReturnType}'.");
+
+                var parseMethodInvokeResult = parseMethod.Invoke(valueToReturn, new[] { tokenValue });
+
+                if (parseMethodInvokeResult == null)
+                {
+                    parseArgumentResult.ErrorMessage = $"[{name}] Cannot parse argument. Format was invalid.";
+                    return null;
+                }
+
+                if (parseMethodInvokeResult is T)
+                {
+                    return (T)parseMethodInvokeResult;
+                }
+
+                try
+                {
+                    return (T)Convert.ChangeType(parseMethodInvokeResult, typeof(T));
+                }
+                catch (InvalidCastException)
+                {
+                    throw;
+                }
+            });
+
+            return new Option<T?>(name, parseArgument, isDefault: false, description)
+            {
+                AllowMultipleArgumentsPerToken = false,
+                Arity = ArgumentArity.ExactlyOne,
+                IsRequired = isRequired
+            };
+        }
+
+        private static Option<string?> CreateSingleTokenOption(string name, bool isRequired = false, string? description = null)
+        {
+            var parseArgument = new ParseArgument<string?>(parseArgumentResult =>
+            {
+                return GetStringFromSingleTokenOption(parseArgumentResult, "FirstName");
+            });
+
+            return new Option<string?>(name, parseArgument, isDefault: false, description)
+            {
+                AllowMultipleArgumentsPerToken = false,
+                Arity = ArgumentArity.ExactlyOne,
+                IsRequired = isRequired
+            };
+        }
+
+        private static string? GetStringFromSingleTokenOption(ArgumentResult result, string optionName)
+        {
+            if (result.Tokens.Count == 0)
+                return null;
+
+            var token = result.Tokens[0];
+
+            if (string.IsNullOrWhiteSpace(token.Value))
+            {
+                result.ErrorMessage = $"[{optionName}] Option argument was null or whitespace.";
+
+                return null;
+            }
+
+            return token.Value;
+        }
+    }
+}
---
+++ iAge.ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using iAge.ConsoleApp.DataAccess;
+using iAge.ConsoleApp.Models;
+
+namespace iAge.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string jsonPath = "employees.json";
+
+            IInvocableProvider<Employee> employeeProvider = new JsonProvider<Employee>(jsonPath);
+
+            var commandLineHandler = new CommandLineHandler();
+
+            if (employeeProvider != null)
+            {
+                commandLineHandler.EmployeeAddEvent += employeeProvider.OnAdd;
+                commandLineHandler.EmployeeUpdateEvent += employeeProvider.OnUpdate;
+                commandLineHandler.EmployeeGetEvent += employeeProvider.OnGet;
+                commandLineHandler.EmployeeDeleteEvent += employeeProvider.OnDelete;
+                commandLineHandler.EmployeeGetAllEvent += employeeProvider.OnGetAll;
+            }
+
+            commandLineHandler.CreateCommandTree();
+            commandLineHandler.RunCommand(args);
+        }
+    }
+}
